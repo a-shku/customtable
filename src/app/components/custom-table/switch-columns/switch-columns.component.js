7f989468a8f435c4c4d2a6213e7514ab6b5f598e
energy1 <vsenetak88@mail.ru>
2017-11-15 13:45:52 +0200

    pluggedIn dialog

--- src/app/components/custom-table/switch-columns/switch-columns.component.js
+++ src/app/components/custom-table/switch-columns/switch-columns.component.js
@@ -6,14 +6,50 @@ const switchColumns = {
     appColumns: '<'
   },
   controller: class SwitchColumnsController {
-    constructor() {
+    constructor($mdDialog) {
+      'ngInject';
       console.log('SWITCH', this.appColumns);
 
+      this.$mdDialog = $mdDialog;
+      this.originatorEv;
+      this.notificationsEnabled = true;
     }
 
     $onChanges() {
       console.log('chh', this.appColumns);
     }
+
+    //     
+
+    openMenu($mdMenu, ev) {
+      console.log('$mdDialog', this.$mdDialog)
+      this.originatorEv = ev;
+      $mdMenu.open(ev)
+    };
+
+
+    toggleNotifications() {
+      this.notificationsEnabled = !this.notificationsEnabled;
+    };
+
+    redial() {
+      this.$mdDialog.show(
+        this.$mdDialog.alert()
+          .targetEvent(this.originatorEv)
+          .clickOutsideToClose(true)
+          .parent('body')
+          .title('Suddenly, a redial')
+          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+          .ok('That was easy')
+      );
+
+      this.originatorEv = null;
+    };
+
+    checkVoicemail() {
+      // This never happens.
+    };
+    //
 
 
   }
